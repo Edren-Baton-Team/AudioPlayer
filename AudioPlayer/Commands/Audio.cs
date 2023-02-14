11bf23255377d805b8811a02955e08040548b21d
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-02-14 16:02:03 +0400

    Update Audio.cs

--- AudioPlayer/Commands/Audio.cs
+++ AudioPlayer/Commands/Audio.cs
@@ -1,200 +1,233 @@
-﻿using CommandSystem;
-using Exiled.API.Features;
 using Exiled.Permissions.Extensions;
-using Mirror;
-using SCPSLAudioApi.AudioCore;
-using System;
+using System.Collections.Generic;
+using CommandSystem;
+using PlayerRoles;
+using RemoteAdmin;
+using System.Linq;
 using System.IO;
 using VoiceChat;
+using Mirror;
+using System;
+using MEC;
+using SCPSLAudioApi.AudioCore;
 
-namespace AudioPlayer.Commands
+namespace AudioPlayer
 {
     [CommandHandler(typeof(RemoteAdminCommandHandler))]
-    public class Audio : ICommand, IUsageProvider
+    public class AudioCommand : ICommand
     {
-        public string Command { get; } = "audio";
+        public static Dictionary<FakeConnection, ReferenceHub> FakeConnections = new Dictionary<FakeConnection, ReferenceHub>();
+        public static Dictionary<int, ReferenceHub> FakeConnectionsIds = new Dictionary<int, ReferenceHub>();
 
-        public string Description { get; } = "Audio Player command";
+        public string Command { get; } = "audio";
 
         public string[] Aliases { get; } = { "au" };
 
-        public string[] Usage { get; set; } = new string[] { "play/stop/volume/spawn/kick/channel/nickname" };
+        public string Description { get; } = "Audio Command";
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
-            Player player = Player.Get(sender);
-            var audioPlayer = AudioPlayerBase.Get(Plugin.plugin.hubPlayer);
-            var hubPlayer = Plugin.plugin.hubPlayer;
-            var BotReady = Plugin.plugin.BotReady;
-            var newPlayer = UnityEngine.Object.Instantiate(NetworkManager.singleton.playerPrefab);
+            if (!(sender is PlayerCommandSender raSender))
+            {
+                response = "Console cant execute this command!";
+                return false;
+            }
+
+            if (!sender.CheckPermission("audioplayer.audio"))
+            {
+                response = "You dont have permission to run this command.";
+                return false;
+            }
+
+            AudioPlayerBase player = AudioPlayerBase.Get(raSender.ReferenceHub);
+
             if (arguments.Count == 0)
             {
                 response = "" +
-                    "\nArgs: " +
-                    "\naudio play {Path} - Playing Audio From Path" +
-                    "\naudio stop - Stop music" +
-                    "\naudio volume {volume} - Set AudioPlayer Volume" +
-                    "\naudio spawn - Spawn AudioPlayer Bot" +
-                    "\naudio destroy - Destroys AudioPlayer Bot" +
-                    "\naudio channel {VoiceChatChannel} - Changes bot VoiceChannel" +
-                    "\naudio nickname {Name} - Sets name of bot";
+                    "\nArgs:" +
+                    "\naudio add {playerid} - spawns a dummyplayer" +
+                    "\naudio destroy {playerid} - Destroys the player" +
+                    "\naudio play {playerid} {index} - starts playing at the given index" +
+                    "\naudio stop {playerid} - Stop music" +
+                    "\naudio volume {playerid} {volume} - sets the volume" +
+                    "\naudio channel {playerid} {VoiceChatChannel} - Changes bot VoiceChannel" +
+                    "\naudio nick {playerid} {Name} - Sets name of bot";
                 return false;
             }
+
             switch (arguments.At(0).ToLower())
             {
+                case "add":
+                case "spawn":
+                case "create":
+                case "fake":
+                    {
+                        int id = int.Parse(arguments.At(1));
+
+                        var newPlayer = UnityEngine.Object.Instantiate(NetworkManager.singleton.playerPrefab);
+
+                        var fakeConnection = new FakeConnection(id);
+
+
+                        var hubPlayer = newPlayer.GetComponent<ReferenceHub>();
+                        FakeConnections.Add(fakeConnection, hubPlayer);
+                        FakeConnectionsIds.Add(id, hubPlayer);
+
+                        NetworkServer.AddPlayerForConnection(fakeConnection, newPlayer);
+                        try
+                        {
+                            hubPlayer.characterClassManager.UserId = $"player{id}@server";
+                        }
+                        catch (Exception)
+                        {
+                        }
+                        hubPlayer.characterClassManager.InstanceMode = ClientInstanceMode.Host;
+                        try
+                        {
+                            hubPlayer.nicknameSync.SetNick($"Dummy player {id}");
+                        }
+                        catch (Exception)
+                        {
+                        }
+                        Timing.CallDelayed(0.1f, () =>
+                        {
+                            hubPlayer.roleManager.ServerSetRole(RoleTypeId.CustomRole, RoleChangeReason.RemoteAdmin);
+                        });
+                    }
+                    break;
+                case "setnick":
+                case "nick":
+                case "name":
+                case "n":
+                    {
+                        if (arguments.Count == 1 || arguments.Count == 2)
+                        {
+                            response = "Usage: audio nick {ID} {Name}";
+                            return false;
+                        }
+                        int id = int.Parse(arguments.At(1));
+                        if (FakeConnectionsIds.TryGetValue(id, out ReferenceHub hub))
+                        {
+                            hub.nicknameSync.SetNick(arguments.At(2));
+                        }
+                    }
+                    break;
                 case "p":
                 case "play":
                     {
-                        if (!sender.CheckPermission("audioplayer.play"))
+                        if (arguments.Count == 1 || arguments.Count == 2)
+                        {
+                            response = "Usage: audio play {ID} {path}";
+                            return false;
+                        }
+                        int id = int.Parse(arguments.At(1));
+                        if (FakeConnectionsIds.TryGetValue(id, out ReferenceHub hub))
+                        {
+                            var audioPlayer = AudioPlayerBase.Get(hub);
+                            string path = arguments.At(2);
+
+                            if (!File.Exists(path))
+                            {
+                                response = $"No files exist inside that path.\nPath: {path}";
+                                return false;
+                            }
+
+                            audioPlayer.Enqueue(path, -1);
+                            audioPlayer.Play(0);
+                        }
+                    }
+                    break;
+                case "s":
+                case "stop":
+                    {
+                        if (!sender.CheckPermission("audioplayer.stop"))
                         {
                             response = "You dont have perms to do that";
                             return false;
                         }
                         if (arguments.Count == 1)
                         {
-                            response = "Usage: audio play {path}";
+                            response = "Usage: audio stop {ID}";
                             return false;
                         }
-
-                        string path = arguments.At(1);
-
-                        if (!File.Exists(path))
+                        int id = int.Parse(arguments.At(1));
+                        if (FakeConnectionsIds.TryGetValue(id, out ReferenceHub hub))
                         {
-                            response = $"No files exist inside that path.\nPath: {path}";
+                            var audioPlayer = AudioPlayerBase.Get(hub);
+                            audioPlayer.Stoptrack(true);
+                        }
+                    }
+                    break;
+                case "kick":
+                case "destroy":
+                    {
+                        if (!sender.CheckPermission("audioplayer.kickbot"))
+                        {
+                            response = "You dont have perms to do that";
                             return false;
                         }
-
-                        audioPlayer.Enqueue(path, -1);
-                        audioPlayer.Volume = 100;
-                        audioPlayer.Play(0);
-
-                        response = "Playing...";
-                        return true;
+                        if (arguments.Count == 1)
+                        {
+                            response = "Usage: audio kick {ID}";
+                            return false;
+                        }
+                        int id = int.Parse(arguments.At(1));
+                        if (FakeConnectionsIds.TryGetValue(id, out ReferenceHub hub))
+                        {
+                            FakeConnections.Remove(FakeConnections.FirstOrDefault(s => s.Value == hub).Key);
+                            FakeConnectionsIds.Remove(id);
+                            NetworkServer.Destroy(hub.gameObject);
+                        }
                     }
-                case "s":
-                case "stop":
-                    if (!sender.CheckPermission("audioplayer.stop"))
-                    {
-                        response = "You dont have perms to do that";
-                        return false;
-                    }
-                    audioPlayer.Stoptrack(true);
-                    response = "Stop";
-                    return true;
+                    break;
                 case "v":
                 case "vol":
                 case "volume":
-                    if (!sender.CheckPermission("audioplayer.volume"))
                     {
-                        response = "You dont have perms to do that";
-                        return false;
+                        if (!sender.CheckPermission("audioplayer.volume"))
+                        {
+                            response = "You dont have perms to do that";
+                            return false;
+                        }
+                        if (arguments.Count == 1 || arguments.Count == 2)
+                        {
+                            response = "Usage: audio volume {ID} {Number}";
+                            return false;
+                        }
+                        int id = int.Parse(arguments.At(1));
+                        if (FakeConnectionsIds.TryGetValue(id, out ReferenceHub hub))
+                        {
+                            var audioPlayer = AudioPlayerBase.Get(hub);
+                            audioPlayer.Volume = Convert.ToInt32(arguments.At(2));
+                        }
                     }
-                    if (arguments.Count == 1)
-                    {
-                        response = "Usage: audio volume {Number}";
-                        return false;
-                    }
-                    if (!float.TryParse(arguments.At(1), out float volume))
-                    {
-                        response = "Couldn't parse that volume, make sure it is a integer between 0 and 100";
-                        return false;
-                    }
-                    audioPlayer.Volume = volume;
-                    response = "Setted up!";
-                    return true;
-                case "add":
-                case "spawn":
-                case "create":
-                    if (!sender.CheckPermission("audioplayer.createbot"))
-                    {
-                        response = "You dont have perms to do that";
-                        return false;
-                    }
-                    if (!BotReady)
-                    {
-                        response = "Bot already exists!";
-                        return false;
-                    }
-                    BotReady = true;
-                    Plugin.SpawnDummy(player.Id, player.Position, player.Rotation, Plugin.plugin.Config.BotName);
-                    response = "Bot was create server";
-                    return true;
-                case "kick":
-                case "destroy":
-                    if (!sender.CheckPermission("audioplayer.kickbot"))
-                    {
-                        response = "You dont have perms to do that";
-                        return false;
-                    }
-                    if (!BotReady)
-                    {
-                        response = "Bot is already missing!";
-                        return false;
-                    }
-                    BotReady = false;
-                    NetworkServer.RemovePlayerForConnection(Plugin.plugin.fakeConnection, newPlayer);
-                    response = "Bot was kick server";
-                    return true;
-                case "c":
+                    break;
+                case "chan":
                 case "channel":
-                    if (!sender.CheckPermission("audioplayer.channel"))
+                case "audiochannel":
                     {
-                        response = "You dont have perms to do that";
-                        return false;
+                        if (!sender.CheckPermission("audioplayer.channel"))
+                        {
+                            response = "You dont have perms to do that";
+                            return false;
+                        }
+                        if (arguments.Count == 1 || arguments.Count == 2)
+                        {
+                            response = "Usage: audio channel {ID} {VoiceChatChannel}";
+                            return false;
+                        }
+
+                        int id = int.Parse(arguments.At(1));
+                        if (FakeConnectionsIds.TryGetValue(id, out ReferenceHub hub))
+                        {
+                            var audioPlayer = AudioPlayerBase.Get(hub);
+                            audioPlayer.BroadcastChannel = (VoiceChatChannel)Enum.Parse(typeof(VoiceChatChannel), arguments.At(2));
+                        }
                     }
-                    if (arguments.Count == 1)
-                    {
-                        response = "Usage: audio channel {VoiceChatChannel}";
-                        return false;
-                    }
-                    switch (arguments.At(1))
-                    {
-                        case "Intercom":
-                            audioPlayer.BroadcastChannel = VoiceChatChannel.Intercom;
-                            break;
-                        case "Mimicry":
-                            audioPlayer.BroadcastChannel = VoiceChatChannel.Mimicry;
-                            break;
-                        case "None":
-                            audioPlayer.BroadcastChannel = VoiceChatChannel.None;
-                            break;
-                        case "Proximity":
-                            audioPlayer.BroadcastChannel = VoiceChatChannel.Proximity;
-                            break;
-                        case "Radio":
-                            audioPlayer.BroadcastChannel = VoiceChatChannel.Radio;
-                            break;
-                        case "RoundSummary":
-                            audioPlayer.BroadcastChannel = VoiceChatChannel.RoundSummary;
-                            break;
-                        case "Scp1576":
-                            audioPlayer.BroadcastChannel = VoiceChatChannel.Scp1576;
-                            break;
-                        case "ScpChat":
-                            audioPlayer.BroadcastChannel = VoiceChatChannel.ScpChat;
-                            break;
-                        case "Spectator":
-                            audioPlayer.BroadcastChannel = VoiceChatChannel.Spectator;
-                            break;
-                    }
-                    response = $"Bot is now broadcasting in the channel - {audioPlayer.BroadcastChannel}";
-                    return true;
-                case "setnick":
-                case "nick":
-                case "name":
-                case "n":
-                    if (arguments.Count == 1)
-                    {
-                        response = "Usage: audio nick {Name}";
-                        return false;
-                    }
-                    hubPlayer.nicknameSync.SetNick(arguments.At(1));
-                    hubPlayer.nicknameSync.name = arguments.At(1);
-                    response = $"Bot nickname has been changed to - {arguments.At(1)}";
-                    return true;
+                    break;
             }
-            response = "Error";
-            return false;
+            response = "Done";
+            return true;
         }
     }
 }
