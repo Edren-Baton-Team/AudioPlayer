7b982e4fc29868c52985af234f57662c89df48bc
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-02-14 16:02:52 +0400

    Add files via upload

---
+++ AudioPlayer/API/API.cs
@@ -0,0 +1,62 @@
+﻿using PlayerRoles.FirstPersonControl;
+using SCPSLAudioApi.AudioCore;
+using Exiled.API.Features;
+using UnityEngine;
+using VoiceChat;
+using Mirror;
+using System.Linq;
+using HarmonyLib;
+
+namespace AudioPlayer.API
+{
+    public class API
+    {
+        public FakeConnection fakeConnection;
+        public ReferenceHub hubPlayer;
+        public AudioPlayerBase audioplayer;
+        public Harmony _harmonyInstance;
+        public Object newPlayer = Object.Instantiate(NetworkManager.singleton.playerPrefab);
+        public static API api;
+        public static void PlayAudioFromFile(string path, float volume, VoiceChatChannel channel)
+        {
+            var audioPlayer = AudioPlayerBase.Get(api.hubPlayer);
+            audioPlayer.Volume = volume;
+            audioPlayer.BroadcastChannel = channel;
+            audioPlayer.Enqueue(path, -1);
+            audioPlayer.Play(0);
+            Log.Debug("Bot AudioPlayer is Ready!");
+        }
+        public static void SpawnDummy()
+        {
+            var newPlayer = Object.Instantiate(NetworkManager.singleton.playerPrefab);
+            int idplayer = Player.List.Count() + 1;
+            api.fakeConnection = new FakeConnection(idplayer++);
+            api.hubPlayer = newPlayer.GetComponent<ReferenceHub>();
+            NetworkServer.AddPlayerForConnection(api.fakeConnection, newPlayer);
+            api.hubPlayer.characterClassManager._privUserId = $"{idplayer}";
+            api.hubPlayer.characterClassManager.InstanceMode = ClientInstanceMode.Unverified;
+            api.hubPlayer.characterClassManager.GodMode = true;
+            api.hubPlayer.TryOverridePosition(new Vector3(0, 0), new Vector3(0, 0));
+            api.hubPlayer.roleManager.ServerSetRole(PlayerRoles.RoleTypeId.Overwatch, PlayerRoles.RoleChangeReason.RemoteAdmin);
+            api.hubPlayer.nicknameSync.DisplayName = Plugin.plugin.Config.BotName;
+            Log.Debug("Bot AudioPlayer is Ready!");
+        }
+        public static void StopAudio()
+        {
+            var audioPlayer = AudioPlayerBase.Get(api.hubPlayer);
+            audioPlayer.Stoptrack(true);
+            Log.Debug("AudioPlayer stopped playing sound");
+        }
+        public static void VolimeAudio(float volume)
+        {
+            var audioPlayer = AudioPlayerBase.Get(api.hubPlayer);
+            audioPlayer.Volume = volume;
+            Log.Debug($"AudioPlayer sound has been changed to - {volume}");
+        }
+        public static void DisconnectDummy()
+        {
+            NetworkServer.DestroyPlayerForConnection(api.fakeConnection);
+            Log.Debug("AudioPlayer bot was kicked out of game!");
+        }
+    }
+}
---
+++ AudioPlayer/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using Exiled.API.Features;
+using SCPSLAudioApi;
+using HarmonyLib;
+using System.IO;
+using System;
+
+namespace AudioPlayer
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Prefix => "AudioPlayer";
+        public override string Name => "AudioPlayer";
+        public override string Author => "Rysik5318 and Mariki";
+        public override System.Version Version { get; } = new System.Version(1, 0, 3);
+
+        public static Plugin plugin;
+
+        public Harmony _harmonyInstance;
+
+        public readonly string AudioPath = Path.Combine(Paths.Plugins, "audio");
+
+        public override void OnEnabled()
+        {
+            try
+            {
+                plugin = this;
+                Startup.SetupDependencies();
+                Extensions.CreateDirectory();
+                Log.Info("Loading AudioPlayer Event...");
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Error loading plugin: {e}");
+            }
+            try
+            {
+                _harmonyInstance = new Harmony($"Rysik5318.{DateTime.UtcNow.Ticks}");
+                _harmonyInstance.PatchAll();
+                Log.Info("Loading AudioPlayer Harmory Patch...");
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Error on patch harmony: {e.Data} -- {e.StackTrace}");
+            }
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            plugin = this;
+            base.OnDisabled();
+        }
+    }
+}
