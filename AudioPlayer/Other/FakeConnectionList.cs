8eb8b27e3226ab46f6d46b0064ed057825b49485
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-03-28 20:42:18 +0400

    Add files via upload

---
+++ AudioPlayer/Other/FakeConnectionList.cs
@@ -0,0 +1,9 @@
+﻿using SCPSLAudioApi.AudioCore;
+namespace AudioPlayer;
+
+public class FakeConnectionList
+{
+    public FakeConnection fakeConnection { get; set; }
+    public ReferenceHub hubPlayer { get; set; }
+    public AudioPlayerBase audioplayer { get; set; }
+}
