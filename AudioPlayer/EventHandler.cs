7fe4bb0e87e68b02feb79f40d32ff9a7c3a49872
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-04-11 17:40:42 +0400

    Add files via upload

---
+++ AudioPlayer/EventHandler.cs
@@ -0,0 +1,134 @@
+﻿using Exiled.API.Features;
+using Mirror;
+using PlayerRoles;
+using SCPSLAudioApi.AudioCore;
+using System;
+using System.Collections.Generic;
+
+namespace AudioPlayer;
+
+public class EventHandler
+{
+    private List<AudioFile> LobbyPlaylist = Plugin.plugin.Config.LobbyPlaylist;
+    public void OnWaitingForPlayers()
+    {
+        if (Plugin.plugin.Config.SpawnBot)
+        {
+            foreach (var cfg in Plugin.plugin.Config.BotsList)
+            {
+                SpawnDummy(cfg.BotName, cfg.ShowPlayerList, cfg.BadgeText, cfg.BadgeColor, cfg.BotId);
+            }
+
+            if (Plugin.plugin.Config.SpecialEventsEnable)
+            {
+                if (LobbyPlaylist.Count > 0)
+                    LobbyPlaylist[UnityEngine.Random.Range(0, LobbyPlaylist.Count)].Play(true);
+            }
+        }
+    }
+    public void SpawnDummy(string name = "Dedicated Server", bool showplayer = false, string badgetext = "AudioPlayer BOT", string bagdecolor = "orange", int id = 99)
+    {
+        if (Plugin.plugin.FakeConnectionsIds.ContainsKey(id))
+        {
+            Log.Error("This id is already in use");
+            return;
+        }
+        var newPlayer = UnityEngine.Object.Instantiate(NetworkManager.singleton.playerPrefab);
+        var fakeConnection = new FakeConnection(id);
+        var hubPlayer = newPlayer.GetComponent<ReferenceHub>();
+        Plugin.plugin.FakeConnectionsIds.Add(id, new FakeConnectionList()
+        {
+            BotID = id,
+            BotName = name,
+            fakeConnection = fakeConnection,
+            audioplayer = AudioPlayerBase.Get(hubPlayer),
+            hubPlayer = hubPlayer,
+        });
+
+        NetworkServer.AddPlayerForConnection(fakeConnection, newPlayer);
+        if (showplayer)
+            hubPlayer._playerId = new RecyclablePlayerId(id);
+        else
+            hubPlayer.characterClassManager._privUserId = $"player{id}@server";
+        hubPlayer.characterClassManager.InstanceMode = ClientInstanceMode.Host;
+        try
+        {
+            hubPlayer.nicknameSync.SetNick(name);
+        }
+        catch (Exception)
+        {
+            //Nice ass
+        }
+        try
+        {
+            hubPlayer.roleManager.ServerSetRole(RoleTypeId.Overwatch, RoleChangeReason.RemoteAdmin);
+        }
+        catch (Exception e)
+        {
+            Log.Error($"Error on {nameof(SpawnDummy)}: Error on set dummy role {e}");
+        }
+        hubPlayer.characterClassManager.GodMode = true;
+        hubPlayer.serverRoles.SetText(badgetext);
+        hubPlayer.serverRoles.SetColor(bagdecolor);
+    }
+    public void OnRoundStarted()
+    {
+        if (Plugin.plugin.LobbySong)
+        {
+            LobbyPlaylist[UnityEngine.Random.Range(0, LobbyPlaylist.Count)].Stop();
+            Plugin.plugin.LobbySong = false;
+        }
+    }
+    public void OnRestartingRound() => Plugin.plugin.FakeConnectionsIds.Clear();
+    //thx for the tip ced777ric#8321
+    public void HandleInstanceModeChange(ReferenceHub arg1, ClientInstanceMode arg2)
+    {
+        foreach (FakeConnectionList fake in Plugin.plugin.FakeConnectionsIds.Values)
+        {
+            if ((arg2 != ClientInstanceMode.Unverified || arg2 != ClientInstanceMode.Host) && fake.hubPlayer == arg1)
+            {
+                Log.Debug($"Replaced instancemode for dummy to host.");
+                arg1.characterClassManager.InstanceMode = ClientInstanceMode.Host;
+            }
+        }
+    }
+    //AudioEvents
+    public void OnFinishedTrack(AudioPlayerBase playerBase, string track, bool directPlay, ref int nextQueuePos)
+    {
+        if (!Round.IsLobby || !Plugin.plugin.Config.SpecialEventsEnable || !Plugin.plugin.LobbySong)
+            return;
+
+        if (LobbyPlaylist.Count > 0)
+            LobbyPlaylist[UnityEngine.Random.Range(0, LobbyPlaylist.Count)].Play(true);
+    }
+    public void OnTrackSelected(AudioPlayerBase playerBase, bool directPlay, int queuePos, ref string track) =>
+        Log.Info("Loading Audio (Debug SCPSLAudioApi)\n" +
+            $"playerBase - {playerBase} \n" +
+            $"directPlay - {directPlay} \n" +
+            $"queuePos - {queuePos} \n" +
+            $"track - {track} \n" +
+            "");
+
+    public void OnTrackLoaded(AudioPlayerBase playerBase, bool directPlay, int queuePos, string track) =>
+        Log.Info($"Play music {directPlay} (Debug SCPSLAudioApi) \n" +
+            $"playerBase - {playerBase}\n" +
+            $"directPlay - {directPlay}\n" +
+            $"queuePos - {queuePos} \n" +
+            $"track - {track} \n" +
+            "");
+
+    public void OnTrackSelecting(AudioPlayerBase playerBase, bool directPlay, ref int queuePos) =>
+        Log.Info($"Selecting Audio.... (Debug SCPSLAudioApi) \n" +
+            $"playerBase - {playerBase} \n" +
+            $"directPlay - {directPlay} \n" +
+            $"queuePos - {queuePos} \n" +
+            "");
+
+    public void OnFinishedTrackLog(AudioPlayerBase playerBase, string track, bool directPlay, ref int nextQueuePos) =>
+        Log.Info($"Track Complete. (Debug SCPSLAudioApi) \n" +
+            $"playerBase - {playerBase} \n" +
+            $"track - {track} \n" +
+            $"directPlay - {directPlay} \n" +
+            $"nextQueuePos - {nextQueuePos} \n" +
+            "");
+}
---
+++ AudioPlayer/Plugin.cs
@@ -0,0 +1,101 @@
+﻿using AudioPlayer.Other.DLC;
+using Exiled.API.Features;
+using SCPSLAudioApi;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AudioPlayer;
+
+public class Plugin : Plugin<Config>
+{
+    public override string Prefix => "AudioPlayer";
+    public override string Name => "AudioPlayer";
+    public override string Author => "Rysik5318 and Mariki";
+    public override Version Version { get; } = new Version(2, 0, 2);
+
+    public static Plugin plugin;
+
+    public Dictionary<int, FakeConnectionList> FakeConnectionsIds = new Dictionary<int, FakeConnectionList>(); // It's more convenient.
+    public EventHandler handlers;
+    public SpecialEvents specialEvents;
+    public bool LobbySong;
+    public readonly string AudioPath = Path.Combine(Paths.Plugins, "audio");
+
+    public override void OnEnabled()
+    {
+        try
+        {
+            plugin = this;
+            FakeConnectionsIds.Clear();
+            handlers = new EventHandler();
+            Exiled.Events.Handlers.Server.WaitingForPlayers += handlers.OnWaitingForPlayers;
+            Exiled.Events.Handlers.Server.RestartingRound += handlers.OnRestartingRound;
+            Exiled.Events.Handlers.Server.RoundStarted += handlers.OnRoundStarted;
+
+            //SpecialEvents
+            if (Config.SpecialEventsEnable)
+            {
+                specialEvents = new SpecialEvents();
+                Exiled.Events.Handlers.Server.RoundStarted += specialEvents.OnRoundStarted;
+                Exiled.Events.Handlers.Server.RoundEnded += specialEvents.OnRoundEnded;
+                Exiled.Events.Handlers.Server.RespawningTeam += specialEvents.OnRespawningTeam;
+                Exiled.Events.Handlers.Warhead.Starting += specialEvents.OnWarheadStarting;
+                Exiled.Events.Handlers.Warhead.Stopping += specialEvents.OnWarheadStopping;
+                Exiled.Events.Handlers.Player.Verified += specialEvents.OnVerified;
+                Exiled.Events.Handlers.Player.Died += specialEvents.OnDied;
+            }
+            //
+
+            //AudioEvents
+            SCPSLAudioApi.AudioCore.AudioPlayerBase.OnFinishedTrack += handlers.OnFinishedTrack;
+            CharacterClassManager.OnInstanceModeChanged += handlers.HandleInstanceModeChange;
+            if (Config.ScpslAudioApiDebug)
+            {
+                SCPSLAudioApi.AudioCore.AudioPlayerBase.OnTrackSelecting += handlers.OnTrackSelecting;
+                SCPSLAudioApi.AudioCore.AudioPlayerBase.OnTrackSelected += handlers.OnTrackSelected;
+                SCPSLAudioApi.AudioCore.AudioPlayerBase.OnTrackLoaded += handlers.OnTrackLoaded;
+                SCPSLAudioApi.AudioCore.AudioPlayerBase.OnFinishedTrack += handlers.OnFinishedTrackLog;
+                Log.Warn($"SCPSLAudioApi Debug Enable!");
+            }
+            //
+
+            Startup.SetupDependencies();
+            Extensions.CreateDirectory();
+            Log.Info("Loading AudioPlayer Event...");
+            Log.Warn("This is the beta version of AudioPlayer!");
+        }
+        catch (Exception e)
+        {
+            Log.Error($"Error loading plugin: {e}");
+        }
+        base.OnEnabled();
+    }
+    public override void OnDisabled()
+    {
+        plugin = null;
+        handlers = null;
+        Exiled.Events.Handlers.Server.RestartingRound -= handlers.OnRestartingRound;
+        Exiled.Events.Handlers.Server.WaitingForPlayers -= handlers.OnWaitingForPlayers;
+        Exiled.Events.Handlers.Server.RoundStarted -= handlers.OnRoundStarted;
+
+        specialEvents = null;
+        Exiled.Events.Handlers.Server.RoundStarted -= specialEvents.OnRoundStarted;
+        Exiled.Events.Handlers.Server.RoundEnded -= specialEvents.OnRoundEnded;
+        Exiled.Events.Handlers.Server.RespawningTeam -= specialEvents.OnRespawningTeam;
+        Exiled.Events.Handlers.Warhead.Starting -= specialEvents.OnWarheadStarting;
+        Exiled.Events.Handlers.Warhead.Stopping -= specialEvents.OnWarheadStopping;
+        Exiled.Events.Handlers.Player.Verified -= specialEvents.OnVerified;
+        Exiled.Events.Handlers.Player.Died -= specialEvents.OnDied;
+
+        SCPSLAudioApi.AudioCore.AudioPlayerBase.OnFinishedTrack -= handlers.OnFinishedTrack;
+        CharacterClassManager.OnInstanceModeChanged -= handlers.HandleInstanceModeChange;
+
+        SCPSLAudioApi.AudioCore.AudioPlayerBase.OnTrackSelecting -= handlers.OnTrackSelecting;
+        SCPSLAudioApi.AudioCore.AudioPlayerBase.OnTrackSelected -= handlers.OnTrackSelected;
+        SCPSLAudioApi.AudioCore.AudioPlayerBase.OnTrackLoaded -= handlers.OnTrackLoaded;
+        SCPSLAudioApi.AudioCore.AudioPlayerBase.OnFinishedTrack -= handlers.OnFinishedTrackLog;
+
+        base.OnDisabled();
+    }
+}
