af143a4b5d6db8332d124d2196d33efd1a801141
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-04-13 23:23:43 +0400

    Add files via upload

---
+++ AudioPlayer/Other/EventsArgs/AudioElevatorUsedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Interfaces;
+
+namespace AudioPlayer.Other.EventsArgs
+{
+    public class AudioElevatorUsedEventArgs : IPlayerEvent
+    {
+        public Player Player { get; }
+        public Lift Lift { get; }
+        public BotsList BotsList { get; }
+        public string Path { get; }
+
+        public AudioElevatorUsedEventArgs(Player player, Lift elevator, BotsList botslist, string path)
+        {
+            Player = player;
+            Lift = elevator;
+            BotsList = botslist;
+            Path = path;
+        }
+    }
+}
