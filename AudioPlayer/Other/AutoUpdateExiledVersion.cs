492d5b557f6ea612efa4cac41b182c4942092ad7
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-05-11 17:55:30 +0400

    Delete AudioPlayer directory

--- AudioPlayer/Other/AutoUpdateExiledVersion.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AutoUpdateExiledVersion
-{
-    public static class AutoUpdateExiledVersion 
-    {
-        public static Version RequiredExiledVersion { get; } = new(6,0,0,0);
-    }
-}
