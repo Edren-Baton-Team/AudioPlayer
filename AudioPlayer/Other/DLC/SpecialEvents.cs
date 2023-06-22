2cf3d8f197dd7cc8a3e6742dd35e79a93659571b
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-06-22 21:26:13 +0400

    v2.1.0

--- AudioPlayer/Other/DLC/SpecialEvents.cs
+++ AudioPlayer/Other/DLC/SpecialEvents.cs
@@ -1,4 +1,4 @@
-﻿using AudioPlayer.Other.EventsArgs;
+using AudioPlayer.Other.EventsArgs;
 using Exiled.API.Features;
 using Exiled.Events.EventArgs.Player;
 using Exiled.Events.EventArgs.Server;
@@ -6,69 +6,76 @@ using Exiled.Events.EventArgs.Warhead;
 using MEC;
 using Respawning;
 using System.Collections.Generic;
+using UnityEngine;
+using static AudioPlayer.Plugin;
 
 
 namespace AudioPlayer.Other.DLC
 {
     public class SpecialEvents
     {
-        private List<AudioFile> ElevatorClip = Plugin.plugin.Config.ElevatorClip;
-        private List<int> ElevatorListPlayer = new List<int>();
+        private List<AudioFile> ElevatorClip = plugin.Config.ElevatorClip;
+        private List<int> ElevatorListPlayer = new();
+        private int WarheadStartBotId = 0;
         // Stole the code from the old AudioPlayer :jermasus:
         public void OnRoundStarted()
         {
-            List<AudioFile> playlist = Plugin.plugin.Config.RoundStartClip;
+            List<AudioFile> playlist = plugin.Config.RoundStartClip;
 
             if (playlist.Count > 0)
-                playlist[UnityEngine.Random.Range(0, playlist.Count)].Play();
+                playlist[Random.Range(0, playlist.Count)].Play();
         }
         public void OnRoundEnded(RoundEndedEventArgs ev)
         {
-            List<AudioFile> playlist = Plugin.plugin.Config.RoundEndClip;
+            List<AudioFile> playlist = plugin.Config.RoundEndClip;
 
             if (playlist.Count > 0)
-                playlist[UnityEngine.Random.Range(0, playlist.Count)].Play();
+                playlist[Random.Range(0, playlist.Count)].Play();
         }
         public void OnRespawningTeam(RespawningTeamEventArgs ev)
         {
             switch (ev.NextKnownTeam)
             {
                 case SpawnableTeamType.ChaosInsurgency:
-                    List<AudioFile> playlist = Plugin.plugin.Config.ChaosSpawnClip;
+                    List<AudioFile> playlist = plugin.Config.ChaosSpawnClip;
 
                     if (playlist.Count > 0)
-                        playlist[UnityEngine.Random.Range(0, playlist.Count)].Play();
+                        playlist[Random.Range(0, playlist.Count)].Play();
                     break;
                 case SpawnableTeamType.NineTailedFox:
-                    List<AudioFile> playlist1 = Plugin.plugin.Config.MtfSpawnClip;
+                    List<AudioFile> playlist1 = plugin.Config.MtfSpawnClip;
 
                     if (playlist1.Count > 0)
-                        playlist1[UnityEngine.Random.Range(0, playlist1.Count)].Play();
+                        playlist1[Random.Range(0, playlist1.Count)].Play();
                     break;
             }
         }
 
         public void OnWarheadStarting(StartingEventArgs ev)
         {
-            List<AudioFile> playlist = Plugin.plugin.Config.WarheadStartingClip;
+            List<AudioFile> playlist = plugin.Config.WarheadStartingClip;
 
             if (playlist.Count > 0)
-                playlist[UnityEngine.Random.Range(0, playlist.Count)].Play();
+            {
+                int random = Random.Range(0, playlist.Count);
+                if (plugin.Config.WarheadStopping)
+                    WarheadStartBotId = playlist[random].BotID;
+                playlist[random].Play();
+            }
         }
 
 
         public void OnWarheadStopping(StoppingEventArgs ev)
         {
-            List<AudioFile> playlist = Plugin.plugin.Config.WarheadStoppingClip;
+            List<AudioFile> playlist = plugin.Config.WarheadStoppingClip;
 
-            if (Plugin.plugin.Config.WarheadStopping)
+            if (plugin.Config.WarheadStopping)
             {
-                playlist.Clear();
-                playlist[UnityEngine.Random.Range(0, playlist.Count)].Stop();
+                API.AudioController.StopAudio(WarheadStartBotId);
                 return;
             }
             if (playlist.Count > 0)
-                playlist[UnityEngine.Random.Range(0, playlist.Count)].Play();
+                playlist[Random.Range(0, playlist.Count)].Play();
         }
         public void OnVerified(VerifiedEventArgs ev)
         {
@@ -80,10 +87,10 @@ namespace AudioPlayer.Other.DLC
                     Timing.RunCoroutine(ElevatorPlayerStatus(ev.Player));
                 }
             });
-            List<AudioFile> playlist = Plugin.plugin.Config.PlayerConnectedServer;
+            List<AudioFile> playlist = plugin.Config.PlayerConnectedServer;
 
             if (playlist.Count > 0)
-                playlist[UnityEngine.Random.Range(0, playlist.Count)].PlayFromFilePlayer(new List<int>() { ev.Player.Id });
+                playlist[Random.Range(0, playlist.Count)].PlayFromFilePlayer(new List<int>() { ev.Player.Id });
         }
         public IEnumerator<float> ElevatorPlayerStatus(Player ply)
         {
@@ -99,10 +106,10 @@ namespace AudioPlayer.Other.DLC
                     if (ElevatorClip.Count > 0)
                     {
                         ElevatorListPlayer.Add(ply.Id);
-                        ElevatorClip[UnityEngine.Random.Range(0, ElevatorClip.Count)].PlayFromFilePlayer(ElevatorListPlayer);
-                        foreach (BotsList bots in Plugin.plugin.Config.BotsList)
+                        ElevatorClip[Random.Range(0, ElevatorClip.Count)].PlayFromFilePlayer(ElevatorListPlayer);
+                        foreach (BotsList bots in plugin.Config.BotsList)
                         {
-                            AudioElevatorStartEventArgs ev = new(ply, ply.Lift, bots, ElevatorClip[UnityEngine.Random.Range(0, ElevatorClip.Count)].Path);
+                            AudioElevatorStartEventArgs ev = new(ply, ply.Lift, bots, ElevatorClip[Random.Range(0, ElevatorClip.Count)].Path);
                         }
                     }
                     Log.Debug("Start Elevator Music");
@@ -112,9 +119,9 @@ namespace AudioPlayer.Other.DLC
                 if (ply.Lift.Status == Interactables.Interobjects.ElevatorChamber.ElevatorSequence.MovingAway)
                 {
                     if (ElevatorClip.Count > 0)
-                        foreach (BotsList bots in Plugin.plugin.Config.BotsList)
+                        foreach (BotsList bots in plugin.Config.BotsList)
                         {
-                            AudioElevatorUsedEventArgs ev = new(ply, ply.Lift, bots, ElevatorClip[UnityEngine.Random.Range(0, ElevatorClip.Count)].Path);
+                            AudioElevatorUsedEventArgs ev = new(ply, ply.Lift, bots, ElevatorClip[Random.Range(0, ElevatorClip.Count)].Path);
                         }
                     Log.Debug("Start Elevator MovingAway");
                 }
@@ -123,10 +130,10 @@ namespace AudioPlayer.Other.DLC
                     if (ElevatorClip.Count > 0)
                     {
                         ElevatorListPlayer.Remove(ply.Id);
-                        ElevatorClip[UnityEngine.Random.Range(0, ElevatorClip.Count)].Stop();
-                        foreach (BotsList bots in Plugin.plugin.Config.BotsList)
+                        ElevatorClip[Random.Range(0, ElevatorClip.Count)].Stop();
+                        foreach (BotsList bots in plugin.Config.BotsList)
                         {
-                            AudioElevatorFinishedEventArgs ev = new(ply, ply.Lift, bots, ElevatorClip[UnityEngine.Random.Range(0, ElevatorClip.Count)].Path);
+                            AudioElevatorFinishedEventArgs ev = new(ply, ply.Lift, bots, ElevatorClip[Random.Range(0, ElevatorClip.Count)].Path);
                         }
                     }
                     Log.Debug("Start Elevator DoorOpening");
@@ -138,19 +145,19 @@ namespace AudioPlayer.Other.DLC
         {
             if (ev1.Player == null || ev1.Attacker == null || ev1.DamageHandler.Type == Exiled.API.Enums.DamageType.Unknown)
                 return;
-            List<AudioFile> playlist = Plugin.plugin.Config.PlayerDiedTargetClip;
-            List<AudioFile> playlist1 = Plugin.plugin.Config.PlayerDiedKillerClip;
+            List<AudioFile> playlist = plugin.Config.PlayerDiedTargetClip;
+            List<AudioFile> playlist1 = plugin.Config.PlayerDiedKillerClip;
             if (playlist.Count > 0)
-                foreach (BotsList bots in Plugin.plugin.Config.BotsList)
+                foreach (BotsList bots in plugin.Config.BotsList)
                 {
-                    playlist[UnityEngine.Random.Range(0, playlist.Count)].PlayFromFilePlayer(new List<int>() { ev1.Player.Id });
-                    AudioPlayerDiedTargetEventArgs ev = new(ev1.Player, bots, playlist[UnityEngine.Random.Range(0, playlist.Count)].Path);
+                    playlist[Random.Range(0, playlist.Count)].PlayFromFilePlayer(new List<int>() { ev1.Player.Id });
+                    AudioPlayerDiedTargetEventArgs ev = new(ev1.Player, bots, playlist[Random.Range(0, playlist.Count)].Path);
                 }
             if (playlist1.Count > 0)
-                foreach (BotsList bots in Plugin.plugin.Config.BotsList)
+                foreach (BotsList bots in plugin.Config.BotsList)
                 {
-                    playlist1[UnityEngine.Random.Range(0, playlist1.Count)].PlayFromFilePlayer(new List<int>() { ev1.Attacker.Id });
-                    AudioPlayerDiedAttackerEventArgs ev = new(ev1.Player, bots, playlist1[UnityEngine.Random.Range(0, playlist1.Count)].Path);
+                    playlist1[Random.Range(0, playlist1.Count)].PlayFromFilePlayer(new List<int>() { ev1.Attacker.Id });
+                    AudioPlayerDiedAttackerEventArgs ev = new(ev1.Player, bots, playlist1[Random.Range(0, playlist1.Count)].Path);
                 }
         }
     }
