3954b032be5b828f87512da89f3413d30099150f
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-06-22 21:26:24 +0400

    v2.1.0

--- AudioPlayer/Plugin.cs
+++ AudioPlayer/Plugin.cs
@@ -14,7 +14,7 @@ public class Plugin : Plugin<Config>
     public override string Prefix => "AudioPlayer";
     public override string Name => "AudioPlayer";
     public override string Author => "Rysik5318 and Mariki";
-    public override Version Version { get; } = new Version(2, 0, 9);
+    public override Version Version { get; } = new Version(2, 1, 0);
     public override Version RequiredExiledVersion { get; } = AutoUpdateExiledVersion.AutoUpdateExiledVersion.RequiredExiledVersion;
 
     public static Plugin plugin; //troll 
