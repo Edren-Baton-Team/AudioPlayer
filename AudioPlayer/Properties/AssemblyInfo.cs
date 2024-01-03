36e4a52d186c138fe8e2a41c38e074a6b7844d3a
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2024-01-04 00:28:49 +0400

    Update AssemblyInfo.cs

--- AudioPlayer/Properties/AssemblyInfo.cs
+++ AudioPlayer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Scp106Containment")]
+[assembly: AssemblyTitle("AudioPlayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Scp106Containment")]
+[assembly: AssemblyProduct("AudioPlayer")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
